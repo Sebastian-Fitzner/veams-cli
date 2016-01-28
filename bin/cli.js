b81440c1a64a0c54cd8bed93ca0509810993298a
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2016-01-28 08:16:30 +0100

    updated cli.js, outsourced update notifier to veams.js, updated inserter

--- bin/cli.js
+++ bin/cli.js
@@ -11,35 +11,12 @@
  * Requirements
  * ============================================== */
 var Helpers = require('../lib/utils/helpers');
-var genPkg = require('../node_modules/generator-veams/package.json');
-var pkg = require('../package.json');
-var updateNotifier = require('update-notifier');
-var Chalk = require('chalk');
+var Veams = require('../lib/veams');
 
 /* ==============================================
  * Update Notifier
  * ============================================== */
-var message = [];
-var genNotifier = updateNotifier({pkg: genPkg, updateCheckInterval: 1000 * 60 * 60 * 24});
-var veamsNotifier = updateNotifier({pkg: pkg, updateCheckInterval: 1000 * 60 * 60 * 24});
-
-if (genNotifier.update || veamsNotifier.update) {
-	message.push(Chalk.gray('Update available for: \n\n'));
-
-	if (genNotifier && genNotifier.update) {
-		message.push(Helpers.extensions.generatorId + Chalk.gray(' (') + Chalk.green.bold(genNotifier.update.latest) + Chalk.gray(') - current: ' + genNotifier.update.current + '\n')
-		);
-	}
-
-	if (veamsNotifier && veamsNotifier.update) {
-		message.push(pkg.name + Chalk.gray(' (') + Chalk.green.bold(veamsNotifier.update.latest) + Chalk.gray(') - current: ' + veamsNotifier.update.current + '\n')
-		);
-	}
-
-	message.push(Chalk.gray('   - Run ' + Chalk.magenta('veams update') + ' to update.\n'));
-
-	Helpers.message('magenta', Helpers.msg.info(message.join(' ')));
-}
+Veams.checkUpdateAvailability();
 
 /* ==============================================
  * Passed arguments
