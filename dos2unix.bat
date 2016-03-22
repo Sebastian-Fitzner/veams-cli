84dc47dcb94948292e605cae937011a6935a7e6c
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2016-03-22 18:46:20 +0100

    added better utility handling

---
+++ dos2unix.bat
@@ -0,0 +1,1 @@
+ dos2unix bin/cli.js bin/commands/add.js bin/commands/help.js bin/commands/install.js bin/commands/new.js bin/commands/update.js bin/commands/version.js lib/veams.js lib/utils/helpers.js
---
+++ test/specs/cli-spec.js
@@ -0,0 +1,11 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var assert = require('chai').assert;
+
+describe('cli', function () {
+
+	it('veams help', function () {
+
+	});
+});
